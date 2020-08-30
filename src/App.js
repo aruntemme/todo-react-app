915f215645a176947d65a7878467402d16aaed29
aruntemme <arungauthamk@gmail.com>
2020-08-30 08:42:57 +0530

    added material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState(["Take meow", " mow mow "]);
+  const [todos, setTodos] = useState([
+    "Take meow",
+    " mow mow ",
+    "wowowowowowowo",
+  ]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    //adding toDo
+
+    event.preventDefault();
+    setTodos([...todos, input]);
+  };
+
+  function setInputFun(event) {
+    setInput(event.target.value);
+  }
 
   return (
     <div className="App">
       <h1>Todo React App 📒</h1>
-      <input />
-      <button>add +</button>
+      <form>
+        <FormControl>
+          <InputLabel>Write a todo</InputLabel>
+          <Input value={input} onChange={setInputFun} />
+        </FormControl>
 
-      <ul>
-        {todos.map((todo) => (
-          <li>{todo}</li>
-        ))}
-      </ul>
+        <Button
+          disabled={!input}
+          variant="contained"
+          color="secondary"
+          onClick={addTodo}
+          type="submit"
+        >
+          Add
+        </Button>
+        <h1>{input}</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
